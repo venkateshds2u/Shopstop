9a61b49a87921939b7f49b1af266cec2984122a6
shauryam <shaurya_mittal@ymail.com>
2016-11-09 20:30:50 -0800

    Header, carousel and modal implemented in bootsrap

---
+++ js/script.js
@@ -0,0 +1,11 @@
+<script type="text/javascript" src="../js/jssor.slider.mini.js"></script>
+<script>
+    jQuery(document).ready(function ($) {
+        var options = {
+            ..
+        };
+            
+        var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+        ...
+    });
+</script>

67304c03e71a6c7322d381dcf8a379fdaba4de99
venkateshds <venkatesh.donthishashidhar@sjsu.edu>
2016-12-10 18:34:06 -0800

    cart

--- cartproducts_display.php
+++ cartproducts_display.php
@@ -24,9 +24,14 @@ error_reporting(0);
 		  	</a>		  	
 		    <a class="nav-item nav-link active ml-2" href="index.php">Home <span class="sr-only">(current)</span></a>
 		    <a class="nav-item nav-link" href="all_products.php">All Products</a>
+		    
+			<a class="nav-item nav-link" href="all_reviews.php">Best Reviewed Products</a>   
+			<a class="nav-item nav-link" href="all_visits.php">Most Visited Products</a>   
 		    <a class="nav-item nav-link" href="#top">Top Rated</a>
 		    <a class="nav-item nav-link" href="#">About</a>
-			<a class="nav-item nav-link float-lg-right" href="cart_products.php">Cart</a>
+
+		    
+			<a class="nav-item nav-link float-lg-right" href="cartproducts_display.php">Cart</a>
 		    <a class="nav-item nav-link float-lg-right" href="#" data-toggle="modal" data-target="#loginModal">Login</a>
 		    <a class="nav-item nav-link float-lg-right" href="#" data-toggle="modal" data-target="#signupModal">Sign Up</a>
 		  </div>
